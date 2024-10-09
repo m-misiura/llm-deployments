093e8f02e3b0f8491c35f0d179a987b154ef64f1
m-misiura <mmisiura@redhat.com>
2024-10-09 16:08:00 +0100

    :sparkles: modified shel script to be compatible with teh grpc endpoint

--- generator/caikit-standalone-deployment/grpc-deployment/curl_model_endpoint.sh
+++ generator/caikit-standalone-deployment/grpc-deployment/curl_model_endpoint.sh
@@ -5,12 +5,11 @@ SA_TOKEN=$(oc create token user-one)
 
 # Get route
 LLM_ROUTE=$(oc get inferenceservice -o jsonpath="{.items[0].status.url}")
+LLM_ROUTE=${LLM_ROUTE#https://}
 MODEL_ID=$(oc get inferenceservice -o jsonpath="{.items[0].metadata.name}")
-
+TEXT_INPUT=$1
 # Run the curl command with the prompt
-curl --json '{
-    "model_id": "'"${MODEL_ID}"'",
-    "inputs": "'"$1"'"
-}' "$LLM_ROUTE/api/v1/task/text-generation" \
+grpcurl -insecure -d '{"text": "'"${TEXT_INPUT}"'"}' \
     -H "Authorization: Bearer ${SA_TOKEN}" \
-    -H "Content-Type: application/json"
+    -H "mm-model-id: ${MODEL_ID}" \
+    ${LLM_ROUTE}:443 caikit.runtime.Nlp.NlpService/TextGenerationTaskPredict
